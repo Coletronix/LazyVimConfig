7ddb247e05062e54aaa0f74fc731199a9a5a746f
Coletronix <colesfirecar@gmail.com>
2023-09-14 06:36:08 -0400

    add theme

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox at start
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
